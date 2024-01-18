6381894af890d27d4da692b1475da780bf728f05
Victor Kruger <victormatheusk@gmail.com>
2024-01-18 17:24:34 -0300

    Until step 9

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
 
 @Component({
@@ -18,23 +19,20 @@ import { HousingLocation } from '../housinglocation';
     </form>
   </section>
   <section class="results">
-    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+    <app-housing-location
+      *ngFor="let housingLocation of housingLocationList"
+      [housingLocation]="housingLocation">
+    </app-housing-location>
   </section>
   `,
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
